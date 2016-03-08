070be6e47d4602113347a8b1f0ec09e7c31f6f63
Christoph Strobl <cstrobl@pivotal.io>
2016-03-08 14:02:30 +0100

    DATAKV-129 - Allow usage of @AliasFor with @KeySpace.
    We now allow using Spring's meta-annotation programming model. Own meta-annotations can be composed using @KeySpace and @AliasFor annotation.
    Example for a custom meta-annotation corresponding to @KeySpace("sessions") when it's used with @SessionsKeyspace class MyEntity {…}:
    
    @Persistent
    @Retention(RetentionPolicy.RUNTIME)
    @Target({ ElementType.TYPE })
    @KeySpace
    public @interface SessionsKeyspace {
    
    	@AliasFor(annotation = KeySpace.class, attribute = "value")
    	String name() default "sessions";
    }
    
    Original pull request: #19.

---
+++ src/test/java/org/springframework/data/keyvalue/TypeWithCustomComposedKeySpaceAnnotationUsingAliasFor.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.keyvalue;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Persistent;
+import org.springframework.util.ObjectUtils;
+
+/**
+ * A {@link Persistent} type with {@link CustomKeySpaceAnnotationWithAliasFor}.
+ * 
+ * @author Christoph Strobl
+ */
+@CustomKeySpaceAnnotationWithAliasFor(name = "aliased")
+public class TypeWithCustomComposedKeySpaceAnnotationUsingAliasFor {
+
+	@Id String id;
+	String name;
+
+	public TypeWithCustomComposedKeySpaceAnnotationUsingAliasFor(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ObjectUtils.nullSafeHashCode(this.id);
+		result = prime * result + ObjectUtils.nullSafeHashCode(this.name);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof TypeWithCustomComposedKeySpaceAnnotationUsingAliasFor)) {
+			return false;
+		}
+		TypeWithCustomComposedKeySpaceAnnotationUsingAliasFor other = (TypeWithCustomComposedKeySpaceAnnotationUsingAliasFor) obj;
+		if (!ObjectUtils.nullSafeEquals(this.id, other.id)) {
+			return false;
+		}
+		if (!ObjectUtils.nullSafeEquals(this.name, other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+}
