66825fc9a047011cfc0c10e50175ed38cd894fe2
hang <hangxiee@gmail.com>
2021-11-20 20:53:45 -0600

    fix flaky tests

--- src/test/java/org/springframework/data/map/MapKeyValueAdapterUnitTests.java
+++ src/test/java/org/springframework/data/map/MapKeyValueAdapterUnitTests.java
@@ -147,8 +147,17 @@ class MapKeyValueAdapterUnitTests {
 
 		CloseableIterator<Map.Entry<Object, Object>> iterator = adapter.entries(COLLECTION_1);
 
-		assertThat(iterator.next()).isEqualTo(new AbstractMap.SimpleEntry<>("1", object1));
-		assertThat(iterator.next()).isEqualTo(new AbstractMap.SimpleEntry<>("2", object2));
+		Map.Entry<Object, Object> entry1 = iterator.next();
+		Map.Entry<Object, Object> entry2 = iterator.next();
+
+		if(entry1.getKey().equals("1")) {
+			assertThat(entry1).isEqualTo(new AbstractMap.SimpleEntry<>("1", object1));
+			assertThat(entry2).isEqualTo(new AbstractMap.SimpleEntry<>("2", object2));
+		} else {
+			assertThat(entry2).isEqualTo(new AbstractMap.SimpleEntry<>("2", object2));
+			assertThat(entry1).isEqualTo(new AbstractMap.SimpleEntry<>("1", object1));
+		}
+
 		assertThat(iterator.hasNext()).isFalse();
 	}
 
@@ -165,7 +174,14 @@ class MapKeyValueAdapterUnitTests {
 
 		CloseableIterator<Map.Entry<Object, Object>> iterator = adapter.entries(COLLECTION_1);
 
-		assertThat(iterator.next()).isEqualTo(new AbstractMap.SimpleEntry<>("1", object1));
+		Map.Entry<Object, Object> entry1 = iterator.next();
+
+		if(entry1.getKey().equals("1")) {
+			assertThat(entry1).isEqualTo(new AbstractMap.SimpleEntry<>("1", object1));
+		} else {
+			assertThat(entry1).isEqualTo(new AbstractMap.SimpleEntry<>("2", object2));
+		}
+
 		assertThat(iterator.hasNext()).isFalse();
 	}
 
