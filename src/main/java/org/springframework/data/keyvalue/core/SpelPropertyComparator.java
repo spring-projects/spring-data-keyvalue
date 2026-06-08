3be91e7a7599d5d08ce3ce77772b6857521e5677
Christoph Strobl <christoph.strobl@broadcom.com>
2026-06-08 15:28:55 +0200

    More efficient comparator logic.
    Apply comparison directly from code instead of having to build an AST for it.
    
    Closes: #684

--- src/main/java/org/springframework/data/keyvalue/core/SpelPropertyComparator.java
+++ src/main/java/org/springframework/data/keyvalue/core/SpelPropertyComparator.java
@@ -123,25 +123,29 @@ public class SpelPropertyComparator<T> implements Comparator<T> {
 	 * @return
 	 */
 	protected String buildExpressionForPath() {
-
-		return String.format("#comparator.compare(#arg1?.%s,#arg2?.%s)", path.replace(".", "?."),
-				path.replace(".", "?."));
+		return String.format("#arg1?.%s", path.replace(".", "?."));
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public int compare(@Nullable T arg1, @Nullable T arg2) {
+
+		Object value1 = getValue(arg1);
+		Object value2 = getValue(arg2);
+
+		return ((Comparator<Object>) (nullsFirst ? NULLS_FIRST : NULLS_LAST)).compare(value1, value2) * (asc ? 1 : -1);
+	}
+
+	private @Nullable Object getValue(@Nullable T arg) {
 
 		SpelExpression expressionToUse = getExpression();
 
-		SimpleEvaluationContext ctx = SimpleEvaluationContext.forReadOnlyDataBinding().withInstanceMethods().build();
-		ctx.setVariable("comparator", nullsFirst ? NULLS_FIRST : NULLS_LAST);
-		ctx.setVariable("arg1", arg1);
-		ctx.setVariable("arg2", arg2);
+		SimpleEvaluationContext ctx = SimpleEvaluationContext.forReadOnlyDataBinding().build();
+		ctx.setVariable("arg1", arg);
 
 		expressionToUse.setEvaluationContext(ctx);
 
-		Integer value = expressionToUse.getValue(Integer.class);
-		return (value != null ? value : 0) * (asc ? 1 : -1);
+		return expressionToUse.getValue();
 	}
 
 	/**
